6749de3fc46ee5e9a5fc366f695118eb1d948ed6
Annica Gehlen <ama.gehlen@gmail.com>
2019-07-19 17:23:52 +0200

    Generalized code for creating dictionaries for data subsamples.

--- auxiliary/auxiliary_misc.py
+++ auxiliary/auxiliary_misc.py
@@ -8,9 +8,10 @@ import statsmodels as sm
 
 def calculate_bin_frequency(data, bins):
     """
+    Calculates the frequency of bins in a dataframe.
     Args:
     data(pd.DataFrame): Dataframe that contains the raw data.
-    bins(column name): Name of column that contains the bins that should be assessed.
+    bins(column): Name of column that contains the bins that should be assessed.
     
     Returns:
     bin_frequency(pd.DataFrame): Dataframe that contains the frequency of each bin in data and and a constant.
@@ -22,3 +23,23 @@ def calculate_bin_frequency(data, bins):
     bin_frequency['const'] = 1
     
     return bin_frequency
+
+
+def create_groups_dict(data, keys, columns):
+    """
+    Function creates a dictionary containing different subsets of a dataset. Subsets are created using dummies. 
+    
+    Args:
+    data(pd.DataFrame): Dataset that should be split into subsets.
+    keys(list): List of keys that should be used in the dataframe.
+    columns(list): List of dummy variables in dataset that are used for creating subsets.
+    
+    Returns:
+    groups_dict(dictionary)
+    """
+    groups_dict = {}
+    
+    for i in range(len(keys)):
+        groups_dict[keys[i]]=data[data[columns[i]] == 1]
+            
+    return groups_dict
